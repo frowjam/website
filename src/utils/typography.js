b4d2df70d5112a07b9922c7c24f7a65ec9c6ec04
Drew Neil <andrew.jr.neil@gmail.com>
2020-01-29 21:19:36 +0000

    Configure typography

---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "12px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Lilita One",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
+  headerWeight: "normal",
+})
+
+export default typography
